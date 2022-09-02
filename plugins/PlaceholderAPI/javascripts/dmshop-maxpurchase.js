71a6cde6a68441f141b899901a6ebc62ede4ce72
cj89898 <cj89898@gmail.com>
2022-09-02 00:34:50 -0500

    Removed unused material

--- plugins/PlaceholderAPI/javascripts/dmshop-maxpurchase.js
+++
@@ -1,4 +0,0 @@
-var cost = "deluxeshop_"+args[0]+",buy%";
-cost = parseFloat(PlaceholderAPI.static.setPlaceholders(BukkitPlayer, "%"+cost));
-var max = (parseFloat("%vault_eco_balance%")/cost).toFixed();
-max >= 2304 ? 2304 : max;
